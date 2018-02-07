dd1a742a4baab0f243eb4aff6fcc556a204ecc8a
Pulkit Valecha <pulkitvalecha98@gmail.com>
2018-02-07 23:35:47 +0530

    added middleware to add user

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const users = require('./data/users')
+const cp = require('./utils/cookieparser')
 
 const app = express();
 app.use(express.json())
@@ -7,10 +9,19 @@ app.use(express.urlencoded({extended: true}))
 app.set("views","views")
 app.set("view engine","hbs")
 
+app.use((req, res, next)=>{
+    let loginCookie = cp.getCookie(req.header('Cookie'),'login')
+    let user  = users.getUserWithToken(loginCookie);
+    req.user = user
+    next();
+
+})
+
 app.use('/',require('./routes/pages').route)
 app.use('/login',require('./routes/login').route)
 app.use('/signup',require('./routes/signup').route)
 app.use('/logout',require('./routes/logout').route)
+
 
 
 app.listen(3434,()=>{
